ffcab24964f0eda04d4fdd45ca8a9b4caf6ed1de
Olena457 <olenademianenko42@gmail.com>
2024-01-25 19:06:13 +0200

    correction errors

--- js/task-3.js
+++ js/task-3.js
@@ -1,8 +1,13 @@
 function getElementWidth(content, padding, border) {
-  let boxsize = ` ${content} + (${padding} + ${border}) * 2 `;
-  console.log((message = boxsize));
-  return message;
+  let contentValue = Number(content.replace('px', ''));
+  let paddingValue = Number(padding.replace('px', ''));
+  let borderValue = Number(border.replace('px', ''));
+
+  let boxSize = contentValue + (paddingValue + borderValue) * 2;
+
+  return boxSize;
 }
+
 console.log(getElementWidth('50px', '8px', '4px')); // 74
 console.log(getElementWidth('60px', '12px', '8.5px')); // 101
 console.log(getElementWidth('200px', '0px', '0px')); // 200
